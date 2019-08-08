b12fa67fccea56f309a6d8262ac029992c996230
sambaiz <godgourd@gmail.com>
2019-08-08 23:59:47 +0900

    TestEventHandlerIssueCommentCreatedDeploy

--- functions/github/eventhandler/issuecomment_test.go
+++ functions/github/eventhandler/issuecomment_test.go
@@ -1,10 +1,87 @@
 package eventhandler
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"testing"
 
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-github/v26/github"
+	"github.com/sambaiz/cdkbot/functions/github/client"
+	githubClientMock "github.com/sambaiz/cdkbot/functions/github/client/mock"
+	cdkMock "github.com/sambaiz/cdkbot/lib/cdk/mock"
+	"github.com/sambaiz/cdkbot/lib/config"
+	configMock "github.com/sambaiz/cdkbot/lib/config/mock"
+	gitMock "github.com/sambaiz/cdkbot/lib/git/mock"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestEventHandlerIssueCommentCreatedDeploy(t *testing.T) {
+	buf, err := ioutil.ReadFile("./test_event/issuecomment_created_deploy.json")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	var hook *github.IssueCommentEvent
+	if err := json.Unmarshal(buf, &hook); err != nil {
+		t.Error(err)
+		return
+	}
+
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	githubClient := githubClientMock.NewMockClienter(ctrl)
+	gliClient := gitMock.NewMockClienter(ctrl)
+	configClient := configMock.NewMockReaderer(ctrl)
+	cdkClient := cdkMock.NewMockClienter(ctrl)
+
+	hash := "hash"
+	repoOwnerName := hook.GetRepo().GetOwner().GetLogin()
+	repoName := hook.GetRepo().GetName()
+	issueNumber := hook.GetIssue().GetNumber()
+	githubClient.EXPECT().CreateStatusOfLatestCommit(
+		ctx, repoOwnerName, repoName, issueNumber, client.StatePending).Return(nil)
+	githubClient.EXPECT().GetPullRequestLatestCommitHash(
+		ctx, repoOwnerName, repoName, issueNumber).Return(hash, nil)
+	gliClient.EXPECT().Clone(
+		"https://github.com/test-user/test-repo.git",
+		clonePath,
+		&hash,
+	).Return(nil)
+	cfg := &config.Config{
+		CDKRoot: ".",
+	}
+	configClient.EXPECT().Read(fmt.Sprintf("%s/cdkbot.yml", clonePath)).Return(cfg, nil)
+	cdkPath := fmt.Sprintf("%s/%s", clonePath, cfg.CDKRoot)
+	cdkClient.EXPECT().Setup(cdkPath).Return(nil)
+
+	// doActionDeploy()
+	result := "result"
+	args := "TestStack"
+	cdkClient.EXPECT().Deploy(cdkPath, args).Return(result, nil)
+	cdkClient.EXPECT().Diff(cdkPath).Return("", false)
+	githubClient.EXPECT().CreateComment(
+		ctx,
+		repoOwnerName,
+		repoName,
+		issueNumber,
+		fmt.Sprintf("### cdk deploy\n```%s```\n%s", result, "All stacks have been deployed :tada:"),
+	).Return(nil)
+	githubClient.EXPECT().CreateStatusOfLatestCommit(
+		ctx, repoOwnerName, repoName, issueNumber, client.StateSuccess).Return(nil)
+
+	eventHandler := &EventHandler{
+		cli:    githubClient,
+		git:    gliClient,
+		config: configClient,
+		cdk:    cdkClient,
+	}
+	assert.Nil(t, eventHandler.IssueComment(ctx, hook))
+}
 
 func TestParseCommand(t *testing.T) {
 	tests := []struct {
